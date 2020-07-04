30fae5d51b0c0924c9063e1f15c37faf1ea8424f
clean_li <clean_li@yahoo.com>
2020-07-04 17:54:06 +0800

    get location provider list

--- app/src/main/java/com/justice/clean/location/location.java
+++ app/src/main/java/com/justice/clean/location/location.java
@@ -1,5 +1,8 @@
 package com.justice.clean.location;
 
+import android.content.Context;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +11,59 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.Manifest;
+import android.util.Log;
+import android.content.pm.PackageManager;
+
+
+import android.Manifest;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.support.v7.appcompat.R.attr.height;
+import static android.support.v7.appcompat.R.attr.switchMinWidth;
 
 public class location extends AppCompatActivity {
+    private static final String TAG = "CleanLOC";
+    private LocationManager lm;
+    private Context mContext;
+    private List<String> prodiverlist;
+    //private PermissionsChecker mPermissionsChecker;
+    String[] perms = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_PHONE_STATE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +76,22 @@ public class location extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                Snackbar.make(view, prodiverlist.toString(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        String serviceString = Context.LOCATION_SERVICE;
+        lm = (LocationManager) getSystemService(serviceString);
+        /*
+        lm = (LocationManager) mContext.getApplicationContext()
+                .getSystemService(Context.LOCATION_SERVICE);
+        */
+
+        prodiverlist = lm.getProviders(true);
+        String provider = LocationManager.GPS_PROVIDER;
     }
 
     @Override
@@ -48,5 +114,27 @@ public class location extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean checkpm(){
+        /*
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (!hasPermissionsGranted(perms)) {
+                mylog("no permission, quit");
+                //set_cam_hint("no permission, open camera failed");
+                return false;
+            } else {
+                mylog("permision granted, continue");
+                return true;
+            }
+        }
+        */
+        return true;
+
+    }
+
+    private void mylog(String l){
+        Log.i(TAG, "|||||||||||||||||||||||||||||||||||||||||||");
+        Log.i(TAG, l);
     }
 }
